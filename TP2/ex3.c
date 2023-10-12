d8ed2e8de00c52f686823c67efd8eb1d8c2b6c75
AmjadGhoufrane <amjad.ghoufrane@etu.univ-lyon1.fr>
2023-10-12 08:53:53 +0200

    TP2-2

--- TP2/ex3.c
+++ TP2/ex3.c
@@ -2,7 +2,17 @@
 
 int main(void)
 {
-    
+    int i;
+    int e;
+
+    i = 1;
+
+    scanf("%d",&e);
+
+    for (i = 1; i <= 10; ++i)
+    {
+        printf("%d x %d = %d \n",i,e,i*e);
+    }
     
     return 0;
 }
