215c0ca8c7de52ca0d9da606a89ea1ed164bf75b
AmjadGhoufrane <amjad.ghoufrane@etu.univ-lyon1.fr>
2023-10-12 09:37:41 +0200

    TP2-4

--- TP2/ex5.c
+++ TP2/ex5.c
@@ -2,7 +2,30 @@
 
 int main(void)
 {
+    int a;
 
+    scanf("%d",&a);
+
+    if (a>6)
+    {
+        printf("erreur\n");
+        return 0;
+    }
+
+    switch( a )
+    {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            printf("Semaine\n");
+            break;
+        case 5:
+        case 6:
+            printf("Weekend\n");
+        
+    }
 
     return 0 ;
 }
